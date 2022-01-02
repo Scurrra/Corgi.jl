dab23b7b92c034dbf886d742271d010c9bd0446b
Scurrra <iscurrra@gmail.com>
2022-01-02 18:11:25 +0300

    PowerTransformer

--- src/preprocessing/Preprocessing.jl
+++ src/preprocessing/Preprocessing.jl
@@ -3,9 +3,11 @@ module Preprocessing
 using LinearAlgebra
 using Statistics
 
-export StandardScaler, MinMaxScaler, fit!, transform!, transform, inverse_transform!, inverse_transform, fit_transform
+export MinMaxScaler, PowerTransformer, StandardScaler
+export fit!, transform!, transform, inverse_transform!, inverse_transform, fit_transform
 
 include("StandardScaler.jl")
 include("MinMaxScaler.jl")
+include("PowerTransformer.jl")
 
 end
