f707a88913f21f8186419dcf474d2d5d740f38d3
Scurrra <iscurrra@gmail.com>
2022-01-05 20:19:01 +0300

    Utils

---
+++ src/utils.jl
@@ -0,0 +1,23 @@
+"""
+    cwr(n::Int, k::Int)
+
+Combinations with repetitions
+"""
+function cwr(n::Int, k::Int)
+    combs = Vector[]
+    buf = fill(1, k)
+
+    while true
+        for i = k:-1:1
+            if buf[i] > n
+                buf[i-1] += 1
+                buf[i:k] .= buf[i-1]
+            end
+        end
+        push!(combs, copy(buf))
+        all(buf .>= n) && break
+        buf[k] += 1
+    end
+
+    return combs
+end
