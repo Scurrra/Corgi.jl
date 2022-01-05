a77a1320d5ee69a3dfa28fce2f95711e0349dc0c
Scurrra <iscurrra@gmail.com>
2022-01-05 10:47:06 +0300

    Empty commit

--- src/preprocessing/Preprocessing.jl
+++ src/preprocessing/Preprocessing.jl
@@ -3,11 +3,9 @@ module Preprocessing
 using LinearAlgebra
 using Statistics
 
-export MinMaxScaler, PowerTransformer, StandardScaler
-export fit!, transform!, transform, inverse_transform!, inverse_transform, fit_transform
+export StandardScaler, MinMaxScaler, fit!, transform!, transform, inverse_transform!, inverse_transform, fit_transform
 
 include("StandardScaler.jl")
 include("MinMaxScaler.jl")
-include("PowerTransformer.jl")
 
 end
