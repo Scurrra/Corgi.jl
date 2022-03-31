aeacc70cb6a801afe500062147e35b4d8423e653
Scurrra <iscurrra@gmail.com>
2022-03-31 15:02:50 +0300

    MinMaxScaler

--- src/preprocessing/MinMaxScaler.jl
+++ src/preprocessing/MinMaxScaler.jl
@@ -1,69 +1,70 @@
 """
-	MinMaxScaler(; dims::Int=1, feature_range::Tuple=(0., 1.))
-	MinMaxScaler(data::AbstractArray; dims::Int=1, feature_range::Tuple=(0., 1.))
+    MinMaxScaler{T, OUTRANGE} <: AbstractTransformer{T, OUTRANGE}
 
-Transform features by scaling each feature to a given range. Create a structure `MinMaxScaler` instance with specified parameters.
-
-`dims` is the dimention of data to be scaled by. `feature_range` is the desired range of transformed data.
+Scaler that transforms `features` data of type `T` by scaling them to fit range `OUTRANGE`
 """
-struct MinMaxScaler
-    dims::Int
-    feature_range::NTuple{2,Float64}
-
+struct MinMaxScaler{T, OUTRANGE} <: AbstractTransformer{T, OUTRANGE}
     min
     max
-
-    MinMaxScaler(; dims::Int = 1, feature_range::NTuple{2,Float64} = (0.0, 1.0)) = new(dims, feature_range)
-
-    function MinMaxScaler(data::AbstractArray; dims::Int = 1, feature_range::NTuple{2,Float64} = (0.0, 1.0))
-        min = minimum(data .|> Float64, dims = dims)
-        max = maximum(data .|> Float64, dims = dims)
-        new(dims, feature_range, min, max)
+    features
+    
+    function MinMaxScaler{T, OUTRANGE}(data::AbstractMatrix{<:Real}; features=:) where {T, OUTRANGE}
+        min = minimum(data .|> Float64, dims=1)
+        max = maximum(data .|> Float64, dims=1)
+        new{T, Float64.(OUTRANGE)}(min, max, features)
     end
 end
+MinMaxScaler{T, OUTRANGE}(data::T; features=:) where {T<:AbstractDataFrame, OUTRANGE} = MinMaxScaler{T, OUTRANGE}(data[!, features] |> Matrix; features=features)
 
 """
-   	fit!(scaler::MinMaxScaler, data::AbstractArray)
-
-Fit `data` using existing `scaler` parameters.
+    MinMaxScaler{T}(data::T; features=:, outrange::NTuple{2, <:Real}=(0., 1.))
+    
+Construct scaler that scales `features` from `data` of type `T` to fit `outrange`
 """
-function fit!(scaler::MinMaxScaler, data::AbstractArray)
-    scaler = MinMaxScaler(data, dims = scaler.dims, feature_range = scaler.feature_range)
-end
-
-"""
-   	transform!(scaler::MinMaxScaler, data::AbstractArray)
-
-Scale features of `data` according to `feature_range`.
-"""
-transform!(scaler::MinMaxScaler, data::AbstractArray) = @.(data = (data - scaler.min) / (scaler.max - scaler.min) * (scaler.feature_range[2] - scaler.feature_range[1]) + scaler.feature_range[1])
+MinMaxScaler{T}(data::T; features=:, outrange::NTuple{2, <:Real}=(0., 1.)) where {T<:AbstractDataFrame} = MinMaxScaler{T, outrange}(data[!, features] |> Matrix; features=features)
+MinMaxScaler{T}(data::T; features=:, outrange::NTuple{2, <:Real}=(0., 1.)) where {T<:AbstractMatrix{<:Real}} = MinMaxScaler{T, outrange}(data[:, features]; features=features)
 
 """
-   	transform(scaler::MinMaxScaler, data::AbstractArray)
-
-Scale features of `data` according to `feature_range`.
+    MinMaxScaler(data::T; features=:, outrange::NTuple{2, <:Real}=(0., 1.))
+    
+Construct scaler that scales `features` from `data` of type `T` to fit `outrange`
 """
-transform(scaler::MinMaxScaler, data::AbstractArray) = transform!(scaler, copy(data))
+MinMaxScaler(data::T; features=:, outrange::NTuple{2, <:Real}=(0., 1.)) where {T} = MinMaxScaler{T}(data; features=features, outrange=outrange)
 
-"""
-   	inverse_transform!(scaler::MinMaxScaler, data::AbstractArray)
-
-Scale back the `data` to the original representation.
-"""
-inverse_transform!(scaler::MinMaxScaler, data::AbstractArray) = @.(data = (data - scaler.feature_range[1]) / (scaler.feature_range[2] - scaler.feature_range[1]) * (scaler.max - scaler.min) + scaler.min)
 
 """
-   	inverse_transform(scaler::MinMaxScaler, data::AbstractArray)
+   	transform!(scaler::MinMaxScaler{T, OUTRANGE}, data::T)
 
-Scale back the `data` to the original representation.
+Scale features of `data` of type `T` according to `OUTRANGE`.
 """
-inverse_transform(scaler::MinMaxScaler, data::AbstractArray) = inverse_transform!(scaler, copy(data))
+#transform!(scaler::MinMaxScaler{T, OUTRANGE}, data::T) where {T, OUTRANGE} = (data[:, scaler.features] .= (data[:, scaler.features] .- scaler.min) ./ (scaler.max .- scaler.min) .* (OUTRANGE[2] - OUTRANGE[1]) .+ OUTRANGE[1])
+transform!(scaler::MinMaxScaler{T, OUTRANGE}, data::T) where {T, OUTRANGE} = @.(data[:, scaler.features] = (data[:, scaler.features] - scaler.min) / (scaler.max - scaler.min) * (OUTRANGE[2] - OUTRANGE[1]) + OUTRANGE[1])
 
 """
-   	fit_transform(data::AbstractArray; dims::Int=1, feature_range::Tuple=(0.0, 1.0))
+   	transform(scaler::MinMaxScaler{T, OUTRANGE}, data::T)
 
-Fit and scale features of `data` according to `feature_range`.
-
-`dims` is the dimention of data to be scaled by. 
+Scale features of `data` of type `T` according to `OUTRANGE`.
 """
-fit_transform(data::AbstractArray; dims::Int = 1, feature_range::NTuple{2,Float64} = (0.0, 1.0)) = transform!(MinMaxScaler(data, dims = dims, feature_range = feature_range), data)
+transform(scaler::MinMaxScaler{T, OUTRANGE}, data::T) where {T, OUTRANGE} = transform!(scaler, copy(data))
+
+"""
+   	inverse_transform!(scaler::MinMaxScaler{T, OUTRANGE}, data::T)
+
+Scale back the `data` of type `T` to the original representation.
+"""
+inverse_transform!(scaler::MinMaxScaler{T, OUTRANGE}, data::T) where {T, OUTRANGE} = @.(data[:, scaler.features] = (data[:, scaler.features] - OUTRANGE[1]) / (OUTRANGE[2] - OUTRANGE[1]) * (scaler.max - scaler.min) + scaler.min)
+
+"""
+   	inverse_transform(scaler::MinMaxScaler{T, OUTRANGE}, data::T)
+
+Scale back the `data` of type `T` to the original representation.
+"""
+inverse_transform(scaler::MinMaxScaler{T, OUTRANGE}, data::T) where {T, OUTRANGE} = inverse_transform!(scaler, copy(data))
+
+
+"""
+   	fit_transform(data::T; features, outrange::NTuple{2,Float64}=(0.0, 1.0))
+
+Fit and scale `features` of `data` of type `T` according to `outrange`.
+"""
+fit_transform(data::T; features=:, outrange::NTuple{2,Float64}=(0.0, 1.0)) where {T, OUTRANGE} = transform!(MinMaxScaler{T, outrange}(data; features=features), data)
--- src/preprocessing/Preprocessing.jl
+++ src/preprocessing/Preprocessing.jl
@@ -7,13 +7,15 @@ using Optim: optimize
 using Statistics
 using Zygote: gradient # to be removed
 
+abstract type AbstractTransformer{T<:Union{AbstractMatrix{<:Real}, AbstractDataFrame}, OUTRANGE} end
+
 export StandardScaler, MinMaxScaler, PowerTransformer, OneHotEncoder, PolynomialFeatures
 export fit!, transform!, transform, inverse_transform!, inverse_transform, fit_transform
 
 include("MinMaxScaler.jl")
-include("PowerTransformer.jl")
-include("StandardScaler.jl")
-include("OneHotEncoder.jl")
-include("PolynomialFeatures.jl")
+# include("PowerTransformer.jl")
+# include("StandardScaler.jl")
+# include("OneHotEncoder.jl")
+# include("PolynomialFeatures.jl")
 
 end
