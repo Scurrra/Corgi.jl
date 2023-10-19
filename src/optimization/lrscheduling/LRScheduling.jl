a5f3c0cbb9cbe9dc8e6c001c4b511a8f94a9ec94
Scurrra <iscurrra@gmail.com>
2023-10-19 09:46:35 +0300

    Learning rate schedulers untested

--- src/optimization/lrscheduling/LRScheduling.jl
+++ src/optimization/lrscheduling/LRScheduling.jl
@@ -10,14 +10,14 @@ abstract type AbstractLRScheduler end
 """
     DiscreteLRScheduler <: AbstractLRScheduler
 
-Discrete learning rate schedulers: 
+Discrete learning rate schedulers: StepLR, MultiStepLR, ConstantLR.
 """
 abstract type DiscreteLRScheduler <: AbstractLRScheduler end
 
 """
     ContinuousLRScheduler <: AbstractLRScheduler
 
-Continuous learning rate schedulers: 
+Continuous learning rate schedulers: LinearLR, ExponentialLR, MultiplicativeLR, LambdaLR, PolynomialLR.
 """
 abstract type ContinuousLRScheduler <: AbstractLRScheduler end
 
