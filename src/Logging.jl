b08c0abe27df3bdf2050e32a2577bd304850ff9d
Scurrra <iscurrra@gmail.com>
2024-01-14 14:51:36 +0300

    AbstractLogger and loggers for optimizers

---
+++ src/Logging.jl
@@ -0,0 +1,41 @@
+module Logging
+
+"""
+    AbstractLogger
+
+Logger base type.
+"""
+abstract type AbstractLogger end
+
+"""
+    NullLogger
+
+Null logger, i.e. no logger placeholder.
+"""
+struct NullLogger <: AbstractLogger end
+(nl::NullLogger)(_...) = undef
+
+"""
+    CostsLogger
+
+Logger that logs only costs while optimization.
+"""
+struct CostsLogger
+    costs::Vector{Float64}
+end
+
+"""
+    call()
+    
+Log something into logger.
+"""
+function (cl::CostsLogger)(cost::Float64)
+    push!(
+        cl.costs,
+        cost
+    )
+end
+
+export AbstractLogger, CostsLogger
+
+end
