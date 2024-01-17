ec986e6eb6dd797aa36e806b6f00ac3f9696a194
Scurrra <iscurrra@gmail.com>
2024-01-17 20:33:41 +0300

    Types subtree for clusterization metrics

--- src/metrics/Metrics.jl
+++ src/metrics/Metrics.jl
@@ -1,3 +1,14 @@
 module Metrics
     
+"""
+    AbstractMetric
+
+Base metric type. There are two ways to obtain metric: using metrc function itself 
+and by constructing metric structure. The second way let's you to pass metric type to some function
+having an ability to check what exactly you pass to it. 
+"""
+abstract type AbstractMetric end
+
+include("clusterization/Clusterization.jl")
+
 end
--- src/metrics/clusterization/Clusterization.jl
+++ src/metrics/clusterization/Clusterization.jl
@@ -1,3 +1,27 @@
-module Clasterization
-    
+module Clusterization
+
+import ..Metrics: AbstractMetric
+
+"""
+    AbstractClusterizationMetric <: AbstractMetric
+
+Base type for clsterization metrics.
+"""
+abstract type AbstractClusterizationMetric <: AbstractMetric end
+
+"""
+    AbstractInnerClusterizationMetric <: AbstractClusterizationMetric
+
+Base type for inner clsterization metrics.
+"""
+abstract type AbstractInnerClusterizationMetric <: AbstractClusterizationMetric end
+
+"""
+    AbstractOuterClusterizationMetric <: AbstractClusterizationMetric
+
+Base type for outer clsterization metrics.
+"""
+abstract type AbstractOuterClusterizationMetric <: AbstractClusterizationMetric end
+
+
 end
